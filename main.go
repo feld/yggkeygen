dd7e2fed4da934e7335430d42e49fd2567f3a5a6
Mark Felder <feld@feld.me>
2025-06-05 10:58:45 -0700

    This was a lie. There never was a -sig flag in this code.

--- main.go
+++ main.go
@@ -3,10 +3,6 @@ This file generates crypto keys.
 It prints out a new set of keys each time if finds a "better" one.
 By default, "better" means a higher NodeID (-> higher IP address).
 This is because the IP address format can compress leading 1s in the address, to increase the number of ID bits in the address.
-
-If run with the "-sig" flag, it generates signing keys instead.
-A "better" signing key means one with a higher TreeID.
-This only matters if it's high enough to make you the root of the tree.
 */
 package main
 
